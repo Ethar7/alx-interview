240509c1b3cd5a388dc1efb84834d734d253a22e
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-13 22:16:42 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -21,10 +21,10 @@ def pascal_triangle(n):
 
     triangle.append([1])
 
-    for i in range(0, n):
+    for i in range(1, n):
         row = [1]
 
-        for j in range(0, i):
+        for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
         row.append(1)

d2c39f737804af27737893987404c412ee321544
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 16:13:15 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
+"""
+Function that returns the perimeter of the island described in grid
+"""
 
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        perimeter=0
-        rows=len(grid)
-        cols=len(grid[0])
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==1:
-                    perimeter+=4
-                    
-                    if i>0 and grid[i-1][j]==1:  # Up
-                        perimeter-=2
-                    if j>0 and grid[i][j-1]==1:  # Left 
-                        perimeter-=2
-        return perimeter
-        
-        
-        (OR)
-        
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        perimeter=0
-        rows=len(grid)
-        cols=len(grid[0])
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==1:
-                    for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                        dx=i+x
-                        dy=j+y
-                        
-                        if dx>=rows or dy>=cols or dx<0 or dy<0 or grid[dx][dy]==0:
-                            perimeter+=1
-        return perimeter
+
+def island_perimeter(grid):
+    '''find permiter'''
+    perimeter = 0
+    width  = len(grid)
+
+    if not grid:
+        return 0
+
+    for row in range(0, width):
+        height = len(grid[row]);
+        if (height <= 2):
+            continue
+        for col in range(0, height):
+            if row == 0 or col == 0 or col == height - 1 or row == width - 1:
+                if (grid[row][col]):
+                    return 0
+
+            if (grid[row][col]):
+                perimeter += grid[row][col - 1] == 0
+                perimeter += grid[row - 1][col] == 0
+                perimeter += grid[row][col + 1] == 0
+                perimeter += grid[row + 1][col] == 0
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
